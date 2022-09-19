3e4c551fac2d9c7d0cd254f6dfad6bcca797de56
vc-vikasdiwakar <vikas.diwakar@viitor.cloud>
2022-09-19 16:38:22 +0530

    init

---
+++ packages/devdojo/calculator/src/CalculatorServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Devdojo\Calculator;
+
+use Illuminate\Support\ServiceProvider;
+
+class CalculatorServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+        $this->app->make('Devdojo\Calculator\CalculatorController');
+        $this->loadViewsFrom(__DIR__.'/views', 'calculator');
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+        include __DIR__.'/routes.php';
+    }
+}
---
+++ packages/devdojo/calculator/src/routes.php
@@ -0,0 +1,8 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+Route::get('calculator', function(){
+	echo 'Hello from the calculator package!';
+});
+Route::get('add/{a}/{b}', 'Devdojo\Calculator\CalculatorController@add');
+Route::get('subtract/{a}/{b}', 'Devdojo\Calculator\CalculatorController@subtract');
