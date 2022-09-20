81ea079a0615486edd16c0ffcaa466672c82f428
vc-vikasdiwakar <vikas.diwakar@viitor.cloud>
2022-09-20 12:41:48 +0530

    changes

--- packages/devdojo/calculator/src/CalculatorServiceProvider.php
+++ packages/devdojo/calculator/src/CalculatorServiceProvider.php
@@ -17,6 +17,8 @@ class CalculatorServiceProvider extends ServiceProvider
         $this->app->make('Devdojo\Calculator\CalculatorController');
         $this->loadViewsFrom(__DIR__.'/views', 'calculator');
         // include __DIR__.'/routes.php';
+        $this->loadRoutesFrom(__DIR__.'/routes.php');
+
 
     }
 
@@ -28,6 +30,6 @@ class CalculatorServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        $this->loadRoutesFrom(__DIR__.'/routes.php');
+        // $this->loadRoutesFrom(__DIR__.'/routes.php');
     }
 }
